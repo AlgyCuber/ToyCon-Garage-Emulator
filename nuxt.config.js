cc91ab680fa4ea25bc14b465aac4d436e68cd8b9
0918nobita <nobita.0918@gmail.com>
2018-04-23 13:24:12 +0900

    Rename the title of this project

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'NintendoLabo-Emulator',
+    title: 'ToyCon-Garage-Emulator',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'NLEmulator', name: 'NLEmulator', content: 'Nintendo LaboのToy-Conガレージで作成するプログラムを、Web上でデバッグできるエミュレータ' }
+      { hid: 'TCGE', name: 'TCGE', content: 'Nintendo Laboの「Toy-Conガレージ」で作成するプログラムを、Web上でデバッグできるエミュレータ' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
