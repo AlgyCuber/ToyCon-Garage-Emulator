3da8a51e202ac1741fd469999fde1806c8c68205
0918nobita <nobita.0918@gmail.com>
2018-04-24 15:32:01 +0900

    Middle クラスに receive, send メソッドを追加した

--- assets/Middle.js
+++ assets/Middle.js
@@ -1,7 +1,15 @@
 import TGNode from '~/assets/TGNode';
 
 export default class Middle extends TGNode {
-	constructor(name) {
+	constructor(/*string*/ name, /*Input*/ dest) {
 		super(name);
+		this.dest = dest;
+		this.charged = false;
+	}
+	receive() {
+		this.charged = true;
+	}
+	send() {
+		this.dest.receive();
 	}
 };
